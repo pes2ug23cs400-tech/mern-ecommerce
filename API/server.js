38c4dcfb3af00902b661acfc864a978b9f137277
pes2ug23cs400-tech <pes2ug23cs400@pesu.pes.edu>
2026-09-22 15:47:57 +0530

    Remove specific CORS options from server.js
    Removed CORS options configuration for a specific origin.

--- API/server.js
+++ API/server.js
@@ -20,13 +20,6 @@ app.use(core({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", core({
-  origin: "https://mern-ecommerce-one-puce.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
 app.use(bodyParser.json());
 
 //home testing route
