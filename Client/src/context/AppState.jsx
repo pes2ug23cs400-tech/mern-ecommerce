9bbbf6f821a722b09be1e8beca5135f03c12d9f0
pes2ug23cs400-tech <pes2ug23cs400@pesu.pes.edu>
2026-09-22 15:18:03 +0530

    Update API URL in AppState component

--- Client/src/context/AppState.jsx
+++ Client/src/context/AppState.jsx
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 function AppState(props) {
   // const url = "https://mern-ecommerce-api.onrender.com/api";
-  const url = "https://mern-ecommerce-3nm8.onrender.com/api";
+  const url = "https://mern-ecommerce-backend-xzyj.onrender.com/api";
 
 
 
